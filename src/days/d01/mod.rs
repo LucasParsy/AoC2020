eb86b58506a78cbcd51ae7269a6dec44bed50af5
Lucas Parsy <lucas.parsy@epitech.eu>
2020-12-25 01:32:03 +0100

    day 1, at last!
    let's initiate this nice projetc that will improve my skills in
    Rust...
    with lame code. I mean it's basic, but it works (and is a tiny bit
    optimized at least?)

--- src/days/d01/mod.rs
+++ src/days/d01/mod.rs
@@ -2,16 +2,31 @@ use crate::myTest;
 
 pub static IMPLEMENTED: bool = true;
 pub static INTERACTIVE: (bool, bool) = (false, false);
-pub static ISP2: bool = false;
 
-pub fn p1(_input: &[String], _interactive: bool) -> i64 {
-    let num: i64 = _input[0].parse().unwrap();
-    num * 2
+pub fn p1(input: &[String], _interactive: bool) -> i64 {
+    let num_vec: Vec<i64> = input.iter().map(|x| x.parse::<i64>().unwrap()).collect();
+    for (i, a) in num_vec.iter().enumerate() {
+        for b in num_vec[i + 1..].iter() {
+            if a + b == 2020 {
+                return a * b;
+            }
+        }
+    }
+    -1
 }
 
-pub fn p2(_input: &[String], _interactive: bool) -> i64 {
-    let num: i64 = _input[0].parse().unwrap();
-    num * 3
+pub fn p2(input: &[String], _interactive: bool) -> i64 {
+    let num_vec: Vec<i64> = input.iter().map(|x| x.parse::<i64>().unwrap()).collect();
+    for (i, a) in num_vec.iter().enumerate() {
+        for (u, b) in num_vec[i + 1..].iter().enumerate() {
+            for c in num_vec[u + 1..].iter() {
+                if a + b +c == 2020 {
+                    return a * b * c;
+                }
+            }
+        }
+    }
+    -1
 }
 
 myTest!();
