474c6f620bf340095cf64fbfefda1e94a8d26ade
Lucas Parsy <lucas.parsy@epitech.eu>
2020-12-25 23:40:10 +0100

    day 3
    nothing special. Rust is a bit hard to check any line in an array, or
    just check a char in a string, but we do with it.

--- src/days/d03/mod.rs
+++ src/days/d03/mod.rs
@@ -1,14 +1,35 @@
-//use crate::myTest;
-
-pub static IMPLEMENTED: bool = false;
+pub static IMPLEMENTED: bool = true;
 pub static INTERACTIVE: (bool, bool) = (false, false);
 
-pub fn p1(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn step_slope(input: &[String], right: usize, down: usize) -> i64 {
+    let mut res = 0;
+    let mut index = 0;
+    let mut line_it = input.iter();
+    let mut line: Option<&String> = line_it.next();
+    while let Some(l) = line {
+        let mod_ind = index % l.len();
+        if l.as_bytes()[mod_ind] as char == '#' {
+            res += 1;
+        }
+
+        index += right;
+        for _ in 0..down {
+            line = line_it.next();
+        }
+    }
+    res
 }
 
-pub fn p2(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn p1(input: &[String], _interactive: bool) -> i64 {
+    return step_slope(&input, 3, 1);
 }
 
-//myTest!();
+pub fn p2(input: &[String], _interactive: bool) -> i64 {
+    let calcs = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let res = calcs.iter().map(|t| step_slope(&input, t.0, t.1));
+    //println!("{:#?}", res.collect());
+    res.fold(1, |x, y| x * y)
+}
+
+use crate::myTest;
+myTest!();
