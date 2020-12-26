b2c72acaa2c59665580fb12805b208c31aaf973b
Lucas Parsy <lucas.parsy@epitech.eu>
2020-12-26 22:51:46 +0100

    day 5
    progressing at good pace currently.

--- src/days/d05/mod.rs
+++ src/days/d05/mod.rs
@@ -1,14 +1,33 @@
-//use crate::myTest;
-
-pub static IMPLEMENTED: bool = false;
+pub static IMPLEMENTED: bool = true;
 pub static INTERACTIVE: (bool, bool) = (false, false);
 
-pub fn p1(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn get_boarding_id(boarding: &str) -> i64 {
+    let binary_str = boarding
+        .replace('B', "1")
+        .replace('F', "0")
+        .replace('R', "1")
+        .replace('L', "0");
+    let (row, column) = binary_str.split_at(7);
+    // BFFFBBFRRR 70 7
+    let row = i64::from_str_radix(row, 2).unwrap();
+    let column = i64::from_str_radix(column, 2).unwrap();
+    row * 8 + column
 }
 
-pub fn p2(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn p1(input: &[String], _interactive: bool) -> i64 {
+    input.iter().map(|l| get_boarding_id(&l)).max().unwrap()
 }
 
-//myTest!();
+pub fn p2(input: &[String], _interactive: bool) -> i64 {
+    let ids: Vec<i64> = input.iter().map(|l| get_boarding_id(&l)).collect();
+    for elem in ids.iter() {
+        if ids.contains(&(elem + 2)) && !ids.contains(&(elem + 1)) {
+            //println!("{}", elem + 1);
+            return elem + 1;
+        }
+    }
+    0
+}
+
+use crate::myTest;
+myTest!();
