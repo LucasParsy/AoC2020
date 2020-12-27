ea5bed8b46ad8119e914f9d2cc55236718576bd3
Lucas Parsy <lucas.parsy@epitech.eu>
2020-12-27 16:42:25 +0100

    day 6

--- src/days/d06/mod.rs
+++ src/days/d06/mod.rs
@@ -1,14 +1,48 @@
-//use crate::myTest;
-
-pub static IMPLEMENTED: bool = false;
+pub static IMPLEMENTED: bool = true;
 pub static INTERACTIVE: (bool, bool) = (false, false);
 
-pub fn p1(_input: &[String], _interactive: bool) -> i64 {
-    1
+extern crate array_tool;
+use array_tool::vec::{Intersect,Uniq};
+
+fn unique_answers(t: &str) -> usize {
+    t.to_string().into_bytes().unique().len()
 }
 
-pub fn p2(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn p1(input: &[String], _interactive: bool) -> i64 {
+    let mut st: String = String::new();
+    let mut res = 0;
+    for l in input.iter() {
+        if l.is_empty() {
+            res += unique_answers(&st);
+            st.clear();
+        } else {
+            st += l;
+        }
+    }
+    res += unique_answers(&st);
+    res as i64
 }
 
-//myTest!();
+pub fn p2(input: &[String], _interactive: bool) -> i64 {
+    let mut st: Vec<u8> = Vec::new();
+    let mut res = 0;
+    let mut first_elem = true;
+    for l in input.iter() {
+        if l.is_empty() {
+            res += st.len();
+            //println!("{:#?}", st);
+            st.clear();
+            first_elem = true;
+        } else if first_elem {
+            first_elem = false;
+            st = l.clone().into_bytes();
+        } else {
+            st = st.intersect(l.clone().into_bytes());
+        }
+    }
+    //println!("{:#?}", st);
+    (res + st.len()) as i64
+}
+
+use crate::myTest;
+myTest!();
