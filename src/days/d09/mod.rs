7f28de8cfac99482c6734a4f4fe16f51d7e1e210
Lucas Parsy <lucas.parsy@epitech.eu>
2020-12-29 15:31:31 +0100

    optimized day 9
    to prevent to parse twice the input on step 2

--- src/days/d09/mod.rs
+++ src/days/d09/mod.rs
@@ -3,10 +3,9 @@ pub static INTERACTIVE: (bool, bool) = (false, false);
 
 extern crate itertools;
 use itertools::Itertools;
-use std::cmp::{Ordering, min, max};
+use std::cmp::{max, min, Ordering};
 
-pub fn p1(input: &[String], _interactive: bool) -> i64 {
-    let nums: Vec<u64> = input.iter().map(|f| f.parse::<u64>().unwrap()).collect();
+fn solve_p1(nums: &[u64]) -> u64 {
     let preamble = if nums.len() < 25 { 5 } else { 25 }; // unit test, for once, are peculiar
     let mut index = preamble;
     loop {
@@ -19,28 +18,31 @@ pub fn p1(input: &[String], _interactive: bool) -> i64 {
         }
         index += 1;
     }
-    nums[index] as i64
+    nums[index]
+}
+
+pub fn p1(input: &[String], _interactive: bool) -> i64 {
+    let nums: Vec<u64> = input.iter().map(|f| f.parse::<u64>().unwrap()).collect();
+    solve_p1(&nums) as i64
 }
 
 pub fn p2(input: &[String], _interactive: bool) -> i64 {
-    let to_find = p1(input, false) as u64;
     let nums: Vec<u64> = input.iter().map(|f| f.parse::<u64>().unwrap()).collect();
-    for i in 0..nums.len()
-    {
+    let to_find = solve_p1(&nums);
+
+    for i in 0..nums.len() {
         let mut sum = 0;
-        let mut n_min = 10000000000; 
-        let mut n_max = 0; 
-        for c  in nums.iter().skip(i)
-        {
+        let mut n_min = 10000000000;
+        let mut n_max = 0;
+        for c in nums.iter().skip(i) {
             n_min = min(n_min, *c);
             n_max = max(n_max, *c);
             sum += c;
             match sum.cmp(&to_find) {
                 Ordering::Less => (),
                 Ordering::Equal => return (n_min + n_max) as i64,
-                Ordering::Greater => continue
+                Ordering::Greater => continue,
             };
-            
         }
     }
     0
