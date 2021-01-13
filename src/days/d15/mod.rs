bcd6c2db393184fe0ccb3a731470204867025010
Lucas Parsy <lucas.parsy@epitech.eu>
2021-01-13 22:13:14 +0100

    day 15
    As my p1 solution was so slow for p2, I was too focused finding an optimal solution that wouldn't loop 30000000 times.
    So just searching for a hint expecting a complex modular arithmetic property like previous exercises, I spoiled myself just by seeing a reddit user saying "I just used hashmap".
    So the answer to improve performance was obvious and I felt dumb not implementing it earlier.
    
    Apparently this is a Van Eck sequence, that can't be simplified any way.
    
    Still on my computer the answer would take 20s to compute.
    That's where I discovered that the "--release" option of cargo is not just for show, and that it optimizes performances 10 times.
    
    Put some tests as "ignored", because each file still takes  2-3s to solve, and I have a pre-commit hook with the tests... I'm bored waiting for tests guaranteed to work.

--- src/days/d15/mod.rs
+++ src/days/d15/mod.rs
@@ -1,14 +1,49 @@
-//use crate::myTest;
+use std::collections::HashMap;
 
-pub static IMPLEMENTED: bool = false;
+pub static IMPLEMENTED: bool = true;
 pub static INTERACTIVE: (bool, bool) = (false, false);
 
-pub fn p1(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn p1(input: &[String], _interactive: bool) -> i64 {
+    let mut nums: Vec<i64> = input[0]
+        .split(',')
+        .map(|n| n.parse::<i64>().unwrap())
+        .collect();
+
+    let mut prev_num = *nums.last().unwrap();
+    for i in nums.len() + 1..=2020 {
+        let mut it = nums.iter().rev().skip(1);
+        let pos = it.position(|n| *n == prev_num);
+        let mut res: i64 = 0;
+        if let Some(n) = pos {
+            res = (i - 1 - (i - 2 - n)) as i64;
+        }
+        nums.push(res);
+        prev_num = res
+    }
+    prev_num
 }
 
-pub fn p2(_input: &[String], _interactive: bool) -> i64 {
-    1
+pub fn p2(input: &[String], _interactive: bool) -> i64 {
+    let mut nums: HashMap<i64, usize> = HashMap::new();
+
+    let mut prev_num = 0;
+    for (index, elem) in input[0].split(',').enumerate() {
+        prev_num = (*elem).parse().unwrap();
+        nums.insert(prev_num, index+1);
+    }
+    nums.remove(&prev_num);
+
+    for i in nums.len() + 1..30000000 {
+        let mut res = 0;
+        if let Some(pos) = nums.get(&prev_num) {
+            res = i - pos;
+        }
+        nums.insert(prev_num, i);
+        prev_num = res as i64;
+        //print!("{} ", prev_num);
+    }
+    prev_num
 }
 
-//myTest!();
+use crate::myTest;
+myTest!();
