c177692e105fa4d3323146333ef8770a12013fc9
Lucas Parsy <lucas.parsy@epitech.eu>
2021-01-14 02:24:10 +0100

    multithreaded running all days
    complicated my life by using messages to still print results in chronological order even when days are executed in parallel
    (for example if I have computed days 10 and 11, I keep the in a map and wait the result of day 09 to show them)
    
    In the end it doesn't even really give a performance boost (yet) with the release build, but hey, I learned a lot about multi-threading in Rust ¯\_(ツ)_/¯

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate indexmap;
 use indexmap::IndexMap;
 
+extern crate rayon;
+use std::sync::mpsc;
+
 #[macro_use]
 extern crate clap;
-use clap::App;
+use clap::{App, ArgMatches};
 
 use days::common::file_to_lines;
 
@@ -21,6 +24,8 @@ struct DayInfo {
     filename: String,
 }
 
+struct DayResult(u8, i64, i64);
+
 type ModuleMap = IndexMap<u8, DayInfo>;
 
 fn map_modules() -> ModuleMap {
@@ -29,15 +34,83 @@ fn map_modules() -> ModuleMap {
     res
 }
 
-fn call_all(m: &ModuleMap) {
-    for t in m.iter() {
-        if let Ok(lines) = file_to_lines(&t.1.filename) {
-            let res = (t.1.p1)(&lines, false);
-            println!("day {:02} part 1: {}", t.0, res);
-            let res = (t.1.p2)(&lines, false);
-            println!("day {:02} part 2: {}", t.0, res);
+fn call_day(t: (&u8, &DayInfo)) -> Option<DayResult> {
+    //println!("day {:02} part 1: {:?}", t.0, &t.1.filename);
+    if let Ok(lines) = file_to_lines(&t.1.filename) {
+        let r1 = (t.1.p1)(&lines, false);
+        let r2 = (t.1.p2)(&lines, false);
+        return Some(DayResult(*t.0, r1, r2));
+    }
+    None
+}
+
+fn call_day_threaded(t: (&u8, &DayInfo), tx: &mpsc::Sender<DayResult>) {
+    if let Some(res) = call_day(t) {
+        tx.send(res).unwrap();
+    }
+}
+
+fn print_day(rec: &DayResult) {
+    println!("day {:02} part 1: {}", rec.0, rec.1);
+    println!("day {:02} part 1: {}", rec.0, rec.2);
+}
+
+fn single_thread_call_all(m: &IndexMap<u8, DayInfo>) {
+    for val in m.iter() {
+        if let Some(res) = call_day(val) {
+            print_day(&res);
         }
     }
+}
+
+// When running on single thread, it appears the pool executes the days in reverse order.
+// so reversed the iterator when thread_num == 1
+fn call_all(m: &mut IndexMap<u8, DayInfo>, thread_num: usize) {
+    if thread_num == 1 {
+        return single_thread_call_all(m);
+    }
+
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(thread_num)
+        .build()
+        .unwrap();
+
+    pool.scope(|s| {
+        let (tx, rx) = mpsc::channel();
+
+        let mut received_map: IndexMap<u8, DayResult> = IndexMap::new();
+        let mut exercices_order_it = m.keys().peekable();
+        for t in m.iter() {
+            let n_tx = tx.clone();
+            s.spawn(move |_| call_day_threaded(t, &n_tx));
+        }
+
+        //order print of days results
+        for rec in rx {
+            match exercices_order_it.peek() {
+                Some(x) if **x == rec.0 => {
+                    print_day(&rec);
+                    exercices_order_it.next();
+                    while let Some(n_ex) = exercices_order_it.peek() {
+                        match received_map.get(*n_ex) {
+                            Some(v) => {
+                                print_day(v);
+                                exercices_order_it.next();
+                            }
+                            None => break,
+                        }
+                    }
+                }
+                Some(_) => {
+                    received_map.insert(rec.0, rec);
+                }
+                None => {}
+            }
+            if exercices_order_it.peek() == None {
+                break;
+            }
+        }
+    });
 }
 
 fn call_precise_exercise(m: &ModuleMap, day: u8, part: u8) {
@@ -72,12 +145,30 @@ fn list_interactive(m: &ModuleMap) {
     }
 }
 
+fn get_num_treads(matches: &ArgMatches) -> usize {
+    match matches.value_of("threads") {
+        Some(threads_str) => match threads_str.parse::<usize>() {
+            Ok(res) => res,
+            Err(_) => {
+                eprintln!(
+                    "invalid thread number '{}', using 1 thread instead",
+                    threads_str
+                );
+                1
+            }
+        },
+        None => 1,
+    }
+}
+
 fn main() {
-    let modules = map_modules();
+    let mut modules = map_modules();
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+    let threads = get_num_treads(&matches);
+
     match matches.subcommand_name() {
-        Some("all") => call_all(&modules),
+        Some("all") => call_all(&mut modules, threads),
         Some("list") => list_exercises(&modules),
         Some("interactives") => list_interactive(&modules),
         Some("day") => {
@@ -87,6 +178,6 @@ fn main() {
             let part = sub.value_of("part").unwrap().parse().unwrap();
             call_precise_exercise(&modules, day, part);
         }
-        _ => call_all(&modules),
+        _ => call_all(&mut modules, threads),
     }
 }
